b2a591ebc57820bd4efcaa0736176720f0858be5
ahmed-al-farouq <mediocre23534@gmail.com>
2025-09-12 11:26:27 +0300

    Update post card component to include a link.

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,12 +1,15 @@
-@props(['title'])
+@props(['title', 'link' => '/'])
 
 <div class="flex flex-col gap-5 py-3 px-4 rounded-xl border-1 border-primary-border">
-    <img src="{{ Vite::asset('resources/images/card-img.webp') }}" alt="{{ $title }}" class="rounded-md" />
+    <a href="{{ $link }}">
+        <img src="{{ Vite::asset('resources/images/card-img.webp') }}" alt="{{ $title }}" class="rounded-md" />
+    </a>
     <div class="ml-2">
         <x-tag size="small">Technology</x-tag>
     </div>
-    <p class="text-white text-2xl font-semibold leading-tight">
+    <a href="{{ $link }}"
+        class="text-white hover:text-primary text-2xl font-semibold leading-tight transition-all duration-200">
         {{ $title }}
-    </p>
+    </a>
     <x-card-footer alt="name" name="Jason Francisco" date="2025-12-25" />
 </div>
